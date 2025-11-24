6aa2d24b1c12c973d466cd83d3407e12620046d9
AlexIves16 <ormix16@gmail.com>
2025-11-25 02:03:34 +0500

    Add debug logging

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -46,15 +46,15 @@ export default defineNuxtConfig({
     turnstileSecretKey: process.env.TURNSTILE_SECRET_KEY,
 
     public: {
-      baseURL: process.env.BASE_URL || 'https://default-url.com',
-      firebaseApiKey: process.env.FIREBASE_API_KEY,
-      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
-      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      firebaseAppId: process.env.FIREBASE_APP_ID,
-      firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
-      turnstileSiteKey: process.env.TURNSTILE_SITE_KEY
+      baseURL: process.env.BASE_URL || 'https://xcargo.kz',
+      firebaseApiKey: process.env.FIREBASE_API_KEY || '',
+      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN || '',
+      firebaseProjectId: process.env.FIREBASE_PROJECT_ID || '',
+      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET || '',
+      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || '',
+      firebaseAppId: process.env.FIREBASE_APP_ID || '',
+      firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID || '',
+      turnstileSiteKey: process.env.TURNSTILE_SITE_KEY || ''
     }
   },
 
--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -1,31 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
-export default defineNuxtPlugin((nuxtApp) => {
-  const config = useRuntimeConfig();
-
-  const firebaseConfig = {
-    apiKey: config.public.firebaseApiKey,
-    authDomain: config.public.firebaseAuthDomain,
-    projectId: config.public.firebaseProjectId,
-    storageBucket: config.public.firebaseStorageBucket,
-    messagingSenderId: config.public.firebaseMessagingSenderId,
-    appId: config.public.firebaseAppId,
-    measurementId: config.public.firebaseMeasurementId
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-  const auth = getAuth(app);
-  const db = getFirestore(app);
-
-  return {
-    provide: {
-      auth,
-      db
+auth,
+  db
     }
   };
 });
