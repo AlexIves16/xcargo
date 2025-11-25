555d8c6c60a3e3dd33e236d34c25e41f112d57ac
AlexIves16 <ormix16@gmail.com>
2025-11-25 16:56:58 +0500

    Relax COOP header to unsafe-none

--- server/middleware/headers.ts
+++ server/middleware/headers.ts
@@ -1,4 +1,4 @@
 export default defineEventHandler((event) => {
-    setResponseHeader(event, 'Cross-Origin-Opener-Policy', 'same-origin-allow-popups')
+    setResponseHeader(event, 'Cross-Origin-Opener-Policy', 'unsafe-none')
     setResponseHeader(event, 'Referrer-Policy', 'no-referrer-when-downgrade')
 })
