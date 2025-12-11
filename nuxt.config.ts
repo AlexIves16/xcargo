86d870869655aa4ffd45612cdd5750e108e27dc3
AlexIves16 <almalexyz@gmail.com>
2025-12-11 20:02:54 +0500

    Fix CSP: Allow Google Fonts and Firebase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,4 +20,11 @@ export default defineNuxtConfig({
       firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
     }
   },
+  routeRules: {
+    '/**': {
+      headers: {
+        'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: blob:; connect-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com https://*.googleapis.com https://*.firebaseio.com https://identitytoolkit.googleapis.com https://*.cloudfunctions.net; frame-src 'self' https://*.firebaseapp.com"
+      }
+    }
+  }
 })
